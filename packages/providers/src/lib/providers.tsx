b57cfb1795e28187f1ddbe56007d3751d22ef643
Enkhtuvshin Narmandakh <37796969+Enkhtuvshin0513@users.noreply.github.com>
2024-11-05 15:24:17 +0800

    add providers

---
+++ packages/providers/src/lib/providers.tsx
@@ -0,0 +1,15 @@
+import styled from 'tailwind';
+
+const StyledProviders = styled.div`
+  color: pink;
+`;
+
+export function Providers() {
+  return (
+    <StyledProviders>
+      <h1>Welcome to Providers!</h1>
+    </StyledProviders>
+  );
+}
+
+export default Providers;
