b4e134f073c36811b4a83b67cc918bb203bf6018
Shinebayar Gansukh <3091558+shinebayar-g@users.noreply.github.com>
2024-11-11 20:13:05 -0800

    connect prisma client

--- apps/backend-next/src/index.ts
+++ apps/backend-next/src/index.ts
@@ -1,9 +1,15 @@
 import express from 'express';
+import { PrismaClient } from '@prisma/client';
 
 const app = express();
-
 const port = 4000;
+const prisma = new PrismaClient();
 
 app.listen(port, () => {
   console.log(`Listening on port http://localhost:${port}`);
+});
+
+app.get('/', async (req, res) => {
+  const c = await prisma.customers.findFirst();
+  res.json(c);
 });
