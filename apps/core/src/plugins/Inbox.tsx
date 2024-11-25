b12f75f8646371d1bd73eb09daf2b650565ae4b9
Enkhtuvshin Narmandakh <37796969+Enkhtuvshin0513@users.noreply.github.com>
2024-11-25 11:26:55 +0800

    merge

--- apps/core/src/plugins/Inbox.tsx
+++ apps/core/src/plugins/Inbox.tsx
@@ -3,7 +3,7 @@ const Inbox = React.lazy(() => import('plugin_inbox/Module'));
 
 export default function InboxPage() {
   return (
-    <React.Suspense fallback="Loading Shop">
+    <React.Suspense fallback="Loading Inbox">
       <Inbox />
     </React.Suspense>
   );
