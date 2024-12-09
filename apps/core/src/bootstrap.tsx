ed275c1aca2436e5e99a1969211e77a2335297eb
Enkhtuvshin Narmandakh <37796969+Enkhtuvshin0513@users.noreply.github.com>
2024-12-09 10:39:52 +0800

    update

--- apps/core/src/bootstrap.tsx
+++ apps/core/src/bootstrap.tsx
@@ -2,21 +2,9 @@ import { StrictMode } from 'react';
 import * as ReactDOM from 'react-dom/client';
 import './styles.css';
 import { App } from '@/app/components/App';
-import { init } from '@module-federation/enhanced/runtime';
 
 // Initialize module federation before rendering
 const initFederation = async () => {
-  init({
-    name: 'core',
-    remotes: [
-      {
-        name: 'plugin_inbox',
-        entry:
-          'https://3b48f883.erxes-next.pages.dev/plugin_inbox/remoteEntry.js',
-      },
-    ],
-  });
-
   const root = ReactDOM.createRoot(
     document.getElementById('root') as HTMLElement
   );
