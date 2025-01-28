89c8915ebacd395fafe8c4c3df87189aac7622a0
Kato <phantompiratefrizy@gmail.com>
2025-01-28 19:36:33 +0800

    refactor: remove unused fields from ADD_CUSTOMERS mutation

--- apps/core/src/modules/contacts/graphql/mutations/addCustomers.ts
+++ apps/core/src/modules/contacts/graphql/mutations/addCustomers.ts
@@ -2,26 +2,16 @@ import { gql } from "@apollo/client";
 
 export const ADD_CUSTOMERS = gql`mutation customersAdd($state: String, $avatar: String, $firstName: String, $lastName: String, $middleName: String, $sex: Int, $birthDate: Date, $primaryEmail: String, $primaryPhone: String, $phones: [String], $emails: [String], $ownerId: String, $position: String, $department: String, $leadStatus: String, $hasAuthority: String, $description: String, $isSubscribed: String, $links: JSON, $customFieldsData: JSON, $code: String, $emailValidationStatus: String, $phoneValidationStatus: String) {
   customersAdd(
-    state: $state
     avatar: $avatar
     firstName: $firstName
     lastName: $lastName
     middleName: $middleName
-    sex: $sex
-    birthDate: $birthDate
     primaryEmail: $primaryEmail
     primaryPhone: $primaryPhone
-    phones: $phones
-    emails: $emails
     ownerId: $ownerId
-    position: $position
-    department: $department
-    leadStatus: $leadStatus
-    hasAuthority: $hasAuthority
     description: $description
     isSubscribed: $isSubscribed
     links: $links
-    customFieldsData: $customFieldsData
     code: $code
     emailValidationStatus: $emailValidationStatus
     phoneValidationStatus: $phoneValidationStatus
