421a5cde7ef62ff193568d243160eea9eccb3a2c
Kato <gundsambuuganzil111@gmail.com>
2025-01-29 22:30:47 +0800

    add user fetching with infinite scroll and search debounce

--- apps/core/src/modules/contacts/hooks/useUsers.tsx
+++ apps/core/src/modules/contacts/hooks/useUsers.tsx
@@ -1,13 +1,40 @@
-import { useQuery } from '@apollo/client';
-import { QueryHookOptions } from '@apollo/client';
-
+import { useQuery, OperationVariables } from '@apollo/client';
 import { GET_USERS } from '@/contacts/graphql/queries/getUsers';
-export const useUsers = (options?: QueryHookOptions) => {
-  const { data, loading } = useQuery(GET_USERS, {
-    ...options
+
+export const useUsers = (options?: OperationVariables) => {
+  const USERS_PER_PAGE = 30;
+  const { data, loading, fetchMore, error } = useQuery(GET_USERS, {
+    ...options,
+    variables: {
+      perPage: USERS_PER_PAGE,
+      ...options?.variables,
+    },
   });
+  const users = data?.users || [];
+  const totalCount = data?.usersTotalCount;
+
+  const handleFetchMore = () => {
+    if (totalCount <= users?.length) return;
+    fetchMore({
+      variables: {
+        ...options?.variables,
+        page: Math.ceil((users?.length || 1) / USERS_PER_PAGE) + 1,
+        perPage: USERS_PER_PAGE,
+      },
+      updateQuery: (prev, { fetchMoreResult }) => {
+        if (!fetchMoreResult) return prev;
+        return Object.assign({}, prev, {
+          users: [...(prev.users || []), ...fetchMoreResult.users],
+        });
+      },
+    });
+  };
+
   return {
-    users: data?.users || [],
-    loading
+    users,
+    loading,
+    handleFetchMore,
+    error,
+    totalCount,
   };
 };
