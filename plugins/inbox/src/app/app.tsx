f2790e7063adb062d1e764fff4fbeabdcd52e0b9
Enkhtuvshin Narmandakh <37796969+Enkhtuvshin0513@users.noreply.github.com>
2025-01-30 16:17:56 +0800

    add plugin inbox

---
+++ plugins/inbox/src/app/app.tsx
@@ -0,0 +1,20 @@
+
+import NxWelcome from "./nx-welcome";
+
+
+
+export function App() {
+
+  return (
+  <div>
+    
+    <NxWelcome title="inbox"/>
+     
+  </div>);
+
+}
+
+
+export default App;
+
+
