758e41d2d9c1d99a91a65626200f74ac1b95211d
Enkhtuvshin Narmandakh <37796969+Enkhtuvshin0513@users.noreply.github.com>
2025-02-02 16:39:16 +0800

    revert folder structure

---
+++ apps/core-ui/module-federation.config.ts
@@ -0,0 +1,33 @@
+// @ts-check
+
+/**
+ * @type {import('@module-federation/sdk').moduleFederationPlugin.ModuleFederationPluginOptions}
+ **/
+
+import { ModuleFederationConfig } from '@nx/rspack/module-federation';
+
+const coreLibraries = new Set([
+  'react',
+  'react-dom',
+  'react-router-dom',
+  'erxes-ui',
+  'recoil',
+  'erxes-shared-states',
+]);
+
+const config: ModuleFederationConfig = {
+  name: 'core-ui',
+
+  shared: (libraryName, defaultConfig) => {
+    if (coreLibraries.has(libraryName)) {
+      return defaultConfig;
+    }
+
+    return false;
+  },
+};
+
+/**
+ * Nx requires a default export of the config to allow correct resolution of the module federation graph.
+ **/
+export default config;
