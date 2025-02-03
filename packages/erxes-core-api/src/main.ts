10cb158b810cdf6c705194654aee862d5c5513ba
Enkhtuvshin Narmandakh <37796969+Enkhtuvshin0513@users.noreply.github.com>
2025-02-03 14:56:39 +0800

    update packages.json

--- packages/erxes-core-api/src/main.ts
+++ packages/erxes-core-api/src/main.ts
@@ -13,7 +13,7 @@ const corsOptions = {
 app.use(cors(corsOptions));
 
 app.get('/', (req, res) => {
-  res.send({ message: 'Hello API' });
+  res.send({ message: 'Hello Cosadsresadsd API' });
 });
 
 app.listen(port, host, () => {
