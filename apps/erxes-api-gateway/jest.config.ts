f6348762986ed3d5e17d609d86db5d90827f5edf
Enkhtuvshin Narmandakh <37796969+Enkhtuvshin0513@users.noreply.github.com>
2025-02-03 13:27:04 +0800

    add gateway

--- apps/erxes-api-gateway/jest.config.ts
+++ apps/erxes-api-gateway/jest.config.ts
@@ -1,10 +1,10 @@
 export default {
-  displayName: 'tasks-api',
-  preset: '../../../jest.preset.js',
+  displayName: 'erxes-api-gateway',
+  preset: '../../jest.preset.js',
   testEnvironment: 'node',
   transform: {
     '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
-  coverageDirectory: '../../../coverage/apps/plugins/tasks-api',
+  coverageDirectory: '../../coverage/apps/erxes-api-gateway',
 };
