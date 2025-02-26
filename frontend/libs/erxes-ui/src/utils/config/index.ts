c56348615dd36cef5c8d800be0ea67f624954e86
Enkhtuvshin Narmandakh <37796969+Enkhtuvshin0513@users.noreply.github.com>
2025-02-26 22:06:23 +0800

    update env

--- frontend/libs/erxes-ui/src/utils/config/index.ts
+++ frontend/libs/erxes-ui/src/utils/config/index.ts
@@ -22,9 +22,9 @@ let memoizedApiUrl: string | null = null;
 const getApi = (): string => {
   if (memoizedApiUrl) return memoizedApiUrl;
 
-  const envApiUrl = window.env?.REACT_APP_API_URL
-    ? process.env.REACT_APP_API_URL || getDefaultUrl()
-    : getDefaultUrl();
+  const envApiUrl =
+    window.env?.REACT_APP_API_URL ??
+    (process.env.REACT_APP_API_URL || getDefaultUrl());
 
   memoizedApiUrl = envApiUrl?.includes('<subdomain>')
     ? envApiUrl.replace('<subdomain>', getSubdomain()).replace('next', 'api')
