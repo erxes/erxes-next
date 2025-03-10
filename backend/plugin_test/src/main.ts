3b35423041653156f14ce3b6ad67cc8b963285f4
Enkhtuvshin Narmandakh <37796969+Enkhtuvshin0513@users.noreply.github.com>
2025-03-10 13:33:45 +0800

    update uitls

---
+++ backend/plugin_test/src/main.ts
@@ -0,0 +1,16 @@
+import express from 'express';
+// import { apiUtils } from 'api-utils';
+
+const host = process.env.HOST ?? 'localhost';
+const port = process.env.PORT ? Number(process.env.PORT) : 3000;
+
+const app = express();
+
+app.get('/', (req, res) => {
+  // console.log(apiUtils());
+  res.send({ message: 'Hello API' });
+});
+
+app.listen(port, host, () => {
+  console.log(`[ ready ] http://${host}:${port}`);
+});
