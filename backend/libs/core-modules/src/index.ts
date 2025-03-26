5ce167c6cb9ab602d63a46a5532f7f8016d95b8d
uuganaa1007 <suuganbayr948@gmail.com>
2025-03-26 12:43:52 +0800

    Moved constants folder to user

--- backend/libs/core-modules/src/index.ts
+++ backend/libs/core-modules/src/index.ts
@@ -3,4 +3,3 @@ export * from './modules/users';
 export * from './modules/common';
 export * from './modules/apps';
 export * from './modules/apps';
-export * from './modules/constants'
--- backend/libs/core-modules/src/modules/users/index.ts
+++ backend/libs/core-modules/src/modules/users/index.ts
@@ -1,1 +1,2 @@
 export * from './db/definitions/users';
+export * from './constants';
