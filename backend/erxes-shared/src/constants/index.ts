f029b887edba6eac33200ebf494c44d64b9d17ac
Enkhtuvshin Narmandakh <37796969+Enkhtuvshin0513@users.noreply.github.com>
2025-04-08 13:07:38 +0800

    add erxes-shared

---
+++ backend/erxes-shared/src/constants/index.ts
@@ -0,0 +1,5 @@
+export { FIELD_FOR_TOTAL_COUNT_AGGREGATE_OPERATION } from './FieldForTotalCountAggregateOperation';
+export { PermissionsOnAllObjectRecords } from './PermissionsOnAllObjectRecords';
+export { STANDARD_OBJECT_RECORDS_UNDER_OBJECT_RECORDS_PERMISSIONS } from './StandardObjectRecordsUnderObjectRecordsPermissions';
+export { ERXES_COMPANIES_BASE_URL } from './ErxesCompaniesBaseUrl';
+export { ERXES_ICONS_BASE_URL } from './ErxesIconsBaseUrl';
