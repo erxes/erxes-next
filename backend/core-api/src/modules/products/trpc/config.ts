77572932477a4710323991f5ac2db4cd9fc8cba6
batmnkh2344 <143669110+batmnkh2344@users.noreply.github.com>
2025-04-24 11:01:04 +0800

    product messagebroker to trpc

---
+++ backend/core-api/src/modules/products/trpc/config.ts
@@ -0,0 +1,21 @@
+import { initTRPC } from '@trpc/server';
+import { z } from 'zod';
+import { ITRPCContext } from '~/init-trpc';
+
+const t = initTRPC.context<ITRPCContext>().create();
+
+export const productConfigTrpcRouter = t.router({
+  productsConfigs: t.router({
+    getConfig: t.procedure.input(z.any()).query(async ({ ctx, input }) => {
+      const { code, defaultValue } = input;
+      const { models } = ctx;
+
+      const productConfig = await models.ProductsConfigs.getConfig(
+        code,
+        defaultValue,
+      );
+
+      return productConfig;
+    }),
+  }),
+});
