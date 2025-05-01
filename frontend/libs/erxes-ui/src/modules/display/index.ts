1a9026beff972f619f7d8706ea0fb951b75c9a2a
Kato-111 <gundsambuuganzil111@gmail.com>
2025-05-01 15:56:59 +0800

    feat: renamed EmailDIsplay with EmailDisplay.tsx

--- frontend/libs/erxes-ui/src/modules/display/index.ts
+++ frontend/libs/erxes-ui/src/modules/display/index.ts
@@ -2,5 +2,5 @@ export * from './components/CurrencyDisplay';
 export * from './components/EllipsisDisplay';
 export * from './components/RelativeDateDisplay';
 export * from './components/PhoneDisplay';
-export * from './components/EmailDIsplay';
+export * from './components/EmailDisplay';
 export * from './components/CurrencyDisplay';
