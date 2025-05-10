a28b70ff3de55603d05fcbb524ed6951b5fa1ea4
Enkhtuvshin0513 <enhtuwshinn@gmail.com>
2025-05-10 20:42:02 +0800

    add users trpc

--- backend/core-api/src/init-trpc.ts
+++ backend/core-api/src/init-trpc.ts
@@ -1,11 +1,14 @@
 import { initTRPC } from '@trpc/server';
 
 import * as trpcExpress from '@trpc/server/adapters/express';
+import { OpenApiMeta } from 'trpc-openapi';
+
 import { getSubdomain } from 'erxes-api-shared/utils';
 
 import { contactRouter } from '~/modules/contacts/trpc';
 import { relationTrpcRouter } from '~/modules/relations/trpc/relation';
 import { conformityTrpcRouter } from '~/modules/conformities/trpc/conformity';
+import { userTrpcRouter } from '~/modules/organization/team-member/trpc/user';
 import { generateModels } from './connectionResolvers';
 
 export const createContext = async ({
@@ -22,12 +25,26 @@ export const createContext = async ({
 
 export type ITRPCContext = Awaited<ReturnType<typeof createContext>>;
 
-const t = initTRPC.context<ITRPCContext>().create();
+const t = initTRPC
+  .meta<OpenApiMeta>()
+  .context<ITRPCContext>()
+  .create({
+    errorFormatter({ shape, error }) {
+      return {
+        ...shape,
+        data: {
+          ...shape.data,
+          customMessage: error.message,
+        },
+      };
+    },
+  });
 
 export const appRouter = t.mergeRouters(
   contactRouter,
   conformityTrpcRouter,
   relationTrpcRouter,
+  userTrpcRouter,
 );
 
 export type AppRouter = typeof appRouter;
