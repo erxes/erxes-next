4f9bbdeeeb16df3a77991be89ba9d907fe160baf
batmnkh2344 <143669110+batmnkh2344@users.noreply.github.com>
2025-05-12 16:32:14 +0800

    fix contact filter

---
+++ backend/plugins/frontline_api/src/modules/integrations/trpc/integration.ts
@@ -0,0 +1,16 @@
+import { initTRPC } from '@trpc/server';
+import { z } from 'zod';
+import { ITRPCContext } from '~/init-trpc';
+
+const t = initTRPC.context<ITRPCContext>().create();
+
+export const integrationRouter = t.router({
+  integration: t.router({
+    find: t.procedure.input(z.any()).query(async ({ ctx, input }) => {
+      const { query } = input;
+      const { models } = ctx;
+
+      return models.Integrations.find(query).lean();
+    }),
+  }),
+});
