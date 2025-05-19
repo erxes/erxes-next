0751f85a6ea269875f6b5b25d4ba1c825379d6fb
batmnkh2344 <143669110+batmnkh2344@users.noreply.github.com>
2025-05-19 22:31:41 +0800

    move permission & add permission to contact

---
+++ backend/core-api/src/modules/permissions/trpc/userGroup.ts
@@ -0,0 +1,16 @@
+import { initTRPC } from '@trpc/server';
+import { z } from 'zod';
+import { ITRPCContext } from '~/init-trpc';
+
+const t = initTRPC.context<ITRPCContext>().create();
+
+export const userGroupTrpcRouter = t.router({
+  userGroups: t.router({
+    getIds: t.procedure.input(z.any()).query(async ({ ctx, input }) => {
+      const { models } = ctx;
+      const { query } = input;
+
+      return await models.UsersGroups.find(query).distinct('_id');
+    }),
+  }),
+});
