b0cd104aa314f2c9de5454d168dedf241f9a3a14
Wlkr7575 <tuguldurch737@gmail.com>
2025-05-20 15:16:41 +0800

    fix: importing moment on automations

--- backend/erxes-api-shared/src/core-modules/automations/utils.ts
+++ backend/erxes-api-shared/src/core-modules/automations/utils.ts
@@ -1,4 +1,4 @@
-import * as moment from 'moment';
+import moment from 'moment';
 import { OPERATORS, staticPlaceholders } from './constants';
 import { pluralFormation, sendWorkerMessage } from '../../utils';
 import {
