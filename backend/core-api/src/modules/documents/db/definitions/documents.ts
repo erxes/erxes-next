8153c60573cb7e535237ee530b8847efffac2b3f
batmnkh2344 <143669110+batmnkh2344@users.noreply.github.com>
2025-05-20 15:19:14 +0800

    move document & add tag meta to core (#135)

---
+++ backend/core-api/src/modules/documents/db/definitions/documents.ts
@@ -0,0 +1,18 @@
+import { Schema } from 'mongoose';
+
+export const documentSchema = new Schema(
+  {
+    createdUserId: { type: String },
+    contentType: { type: String },
+    subType: { type: String, optional: true },
+    name: { type: String },
+    content: { type: String },
+    replacer: { type: String },
+    code: { type: String },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+documentSchema.index({ code: 1 }, { unique: true });
