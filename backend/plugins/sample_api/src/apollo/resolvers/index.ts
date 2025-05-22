06f2181a0f0cc3d88ec3eb286434f1d4eec5099b
Enkhtuvshin Narmandakh <37796969+Enkhtuvshin0513@users.noreply.github.com>
2025-05-22 20:50:08 +0800

    remove sample

--- backend/plugins/sample_api/src/apollo/resolvers/index.ts
+++
@@ -1,9 +0,0 @@
-import { apolloCustomScalars } from 'erxes-api-shared/utils';
-import { customResolvers } from './resolvers';
-
-const resolvers: any = {
-  ...apolloCustomScalars,
-  ...customResolvers,
-};
-
-export default resolvers;
