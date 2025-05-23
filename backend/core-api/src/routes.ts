0907e03807beac88db038f74e1fe976ab01ad3cf
batmnkh2344 <143669110+batmnkh2344@users.noreply.github.com>
2025-05-23 21:54:38 +0800

    feat: file handler

--- backend/core-api/src/routes.ts
+++ backend/core-api/src/routes.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import { router as organizationRoutes } from '~/modules/organization/routes';
+import { router as fileRoutes } from '~/routes/fileRoutes';
 
 const router: Router = Router();
 
 router.use(organizationRoutes);
+router.use(fileRoutes);
 
 export { router };
