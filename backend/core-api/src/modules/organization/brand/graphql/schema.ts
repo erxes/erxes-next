961225b7d1bcdf1ec2dcdc6c02749aed8f887cb3
batmnkh2344 <143669110+batmnkh2344@users.noreply.github.com>
2025-05-30 13:30:33 +0800

    add cursorMode to cursor pagination

--- backend/core-api/src/modules/organization/brand/graphql/schema.ts
+++ backend/core-api/src/modules/organization/brand/graphql/schema.ts
@@ -1,3 +1,5 @@
+import { GQL_CURSOR_PARAM_DEFS } from "erxes-api-shared/utils";
+
 export const types = `
   type Brand @key(fields: "_id") @cacheControl(maxAge: 3) {
     _id: String!
@@ -8,6 +10,8 @@ export const types = `
     createdAt: Date
     emailConfig: JSON
     memberIds: [String]
+
+    cursor: String
   }
 
   type BrandListResponse {
@@ -19,9 +23,7 @@ export const types = `
 
 const queryParams = `
   searchValue: String
-  limit: Int
-  cursor: String
-  direction: CURSOR_DIRECTION
+  ${GQL_CURSOR_PARAM_DEFS}
 `;
 
 export const queries = `
