6151275c069c6387aba19679cbd586ebdb4321c3
Khe <baterdne.k@gmail.com>
2025-06-06 18:00:19 +0800

    arrow based navigatio

--- frontend/plugins/accounting_ui/src/modules/settings/account/components/SelectAccount.tsx
+++ frontend/plugins/accounting_ui/src/modules/settings/account/components/SelectAccount.tsx
@@ -1,9 +1,13 @@
 import {
+  cn,
   Combobox,
   Command,
+  Form,
   Popover,
+  PopoverScoped,
   Skeleton,
   TextOverflowTooltip,
+  usePreviousHotkeyScope,
 } from 'erxes-ui';
 import { useState } from 'react';
 import { useAccounts } from '../hooks/useAccounts';
@@ -19,63 +23,102 @@ export const SelectAccount = React.forwardRef<
     onValueChange: (value: string) => void;
     onCallback?: (account: IAccount) => void;
     defaultFilter: { [key: string]: string | boolean | string[] };
+    inForm?: boolean;
+    scope?: string;
   }
->(({ value, onValueChange, onCallback, defaultFilter, ...props }, ref) => {
-  const [open, setOpen] = useState(false);
-  const [search, setSearch] = useState('');
-  const [debouncedSearch] = useDebounce(search, 200);
-  const { accounts, loading, error, totalCount, handleFetchMore } = useAccounts({
-    variables: {
-      ...defaultFilter,
-      searchValue: debouncedSearch
+>(
+  (
+    {
+      value,
+      onValueChange,
+      onCallback,
+      defaultFilter,
+      inForm,
+      scope,
+      className,
+      ...props
     },
-    skip: (!value && !open),
-  });
+    ref,
+  ) => {
+    const [open, setOpen] = useState(false);
+    const [search, setSearch] = useState('');
+    const [debouncedSearch] = useDebounce(search, 200);
+    const { accounts, loading, error, totalCount, handleFetchMore } =
+      useAccounts({
+        variables: {
+          ...defaultFilter,
+          searchValue: debouncedSearch,
+        },
+        skip: !value && !open,
+      });
 
-  return (
-    <Popover open={open} onOpenChange={setOpen}>
-      <Combobox.Trigger ref={ref} {...props}>
-        {loading ? (
-          <Skeleton className="w-full h-4" />
-        ) : (
-          <Combobox.Value
-            placeholder="Select account"
-            value={
-              accounts?.find((account: IAccount) => account._id === value)?.name
-            }
-          />
-        )}
-      </Combobox.Trigger>
-      <Combobox.Content>
-        <Command shouldFilter={false}>
-          <Command.Input
-            placeholder="Search account"
-            value={search}
-            onValueChange={(v) => setSearch(v)}
-          />
-          <Command.List>
-            <Combobox.Empty loading={loading} error={error} />
-            {accounts?.map((account: IAccount) => (
-              <Command.Item
-                key={account._id}
-                value={account._id}
-                onSelect={() => {
-                  onValueChange(account._id);
-                  onCallback && onCallback(account)
-                  setOpen(false);
-                }}
-              >
-                <TextOverflowTooltip value={`${account.code} - ${account.name}`} />
-                <Combobox.Check checked={account._id === value} />
-              </Command.Item>
-            ))}
+    const Controller = inForm ? Form.Control : React.Fragment;
 
-            <Combobox.FetchMore totalCount={totalCount} currentLength={accounts?.length ?? 0} fetchMore={handleFetchMore} />
-          </Command.List>
-        </Command>
-      </Combobox.Content>
-    </Popover>
-  );
-});
+    const { goBackToPreviousHotkeyScope } = usePreviousHotkeyScope();
+
+    return (
+      <PopoverScoped
+        open={open}
+        onOpenChange={setOpen}
+        scope={scope ? scope + '-select-account' : undefined}
+      >
+        <Controller>
+          <Combobox.Trigger
+            ref={ref}
+            {...props}
+            className={cn('bg-background', className)}
+          >
+            {loading ? (
+              <Skeleton className="w-full h-4" />
+            ) : (
+              <Combobox.Value
+                placeholder="Select account"
+                value={
+                  accounts?.find((account: IAccount) => account._id === value)
+                    ?.name
+                }
+              />
+            )}
+          </Combobox.Trigger>
+        </Controller>
+        <Combobox.Content>
+          <Command shouldFilter={false}>
+            <Command.Input
+              placeholder="Search account"
+              value={search}
+              onValueChange={(v) => setSearch(v)}
+            />
+            <Command.List>
+              <Combobox.Empty loading={loading} error={error} />
+              {accounts?.map((account: IAccount) => (
+                <Command.Item
+                  key={account._id}
+                  value={account._id}
+                  onSelect={() => {
+                    onValueChange(account._id);
+                    onCallback && onCallback(account);
+                    setOpen(false);
+                    goBackToPreviousHotkeyScope();
+                  }}
+                >
+                  <TextOverflowTooltip
+                    value={`${account.code} - ${account.name}`}
+                  />
+                  <Combobox.Check checked={account._id === value} />
+                </Command.Item>
+              ))}
+
+              <Combobox.FetchMore
+                totalCount={totalCount}
+                currentLength={accounts?.length ?? 0}
+                fetchMore={handleFetchMore}
+              />
+            </Command.List>
+          </Command>
+        </Combobox.Content>
+      </PopoverScoped>
+    );
+  },
+);
 
 SelectAccount.displayName = 'SelectAccount';
