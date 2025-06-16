3b131816f2e7fe97e6bf89723262ce582312df0c
soyombo <e11iot.soko@gmail.com>
2025-06-16 16:01:57 +0800

    Update init-trpc.ts

--- backend/plugins/content_api/src/trpc/init-trpc.ts
+++ backend/plugins/content_api/src/trpc/init-trpc.ts
@@ -1,28 +1,15 @@
 import { initTRPC } from '@trpc/server';
 
-import * as trpcExpress from '@trpc/server/adapters/express';
-import { getSubdomain } from 'erxes-api-shared/utils';
+import { ITRPCContext } from 'erxes-api-shared/utils';
 
-import { generateModels } from '~/connectionResolvers';
-// import { inboxTrpcRouter } from './modules/inbox/trpc/inbox';
-// import { integrationTrpcRouter } from './modules/integrations/trpc/integration';
+import { IModels } from '~/connectionResolvers';
 
-export const createContext = async ({
-  req,
-}: trpcExpress.CreateExpressContextOptions) => {
-  const subdomain = getSubdomain(req);
-  const models = await generateModels(subdomain);
+import { portalRouter } from '~/modules/portal/trpc/portal';
 
-  return {
-    subdomain,
-    models,
-  };
-};
+export type ContentTRPCContext = ITRPCContext<{ models: IModels }>;
 
-export type ITRPCContext = Awaited<ReturnType<typeof createContext>>;
+const t = initTRPC.context<ContentTRPCContext>().create();
 
-const t = initTRPC.context<ITRPCContext>().create();
-
-export const appRouter = t.mergeRouters();
+export const appRouter = t.mergeRouters(portalRouter);
 
 export type AppRouter = typeof appRouter;
