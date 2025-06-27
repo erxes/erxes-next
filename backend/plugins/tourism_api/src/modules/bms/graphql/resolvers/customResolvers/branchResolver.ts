9cfba387b165869241cce37c091f38d3a659f51c
orshih <orshih943@gmail.com>
2025-06-27 15:51:15 +0800

    fix(tourism): update

---
+++ backend/plugins/tourism_api/src/modules/bms/graphql/resolvers/customResolvers/branchResolver.ts
@@ -0,0 +1,20 @@
+import { IContext } from '~/connectionResolvers';
+import { sendTRPCMessage } from 'erxes-api-shared/utils';
+
+const item = {
+  async user(branch: any, _args, { models, subdomain }: IContext) {
+    const userDetail = await sendTRPCMessage({
+      pluginName: 'core',
+      method: 'query',
+      module: 'users',
+      action: 'findOne',
+      input: {
+        _id: branch.userId,
+      },
+      defaultValue: {},
+    });
+    return userDetail;
+  },
+};
+
+export default item;
