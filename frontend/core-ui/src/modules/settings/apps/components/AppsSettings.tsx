c579c3c3371ebddb38341199981536a27539000a
Bilguun Munkhzul <kazue@Bilguuns-MacBook-Pro.local>
2025-07-24 11:12:57 +0800

    chore: fix

--- frontend/core-ui/src/modules/settings/apps/components/AppsSettings.tsx
+++ frontend/core-ui/src/modules/settings/apps/components/AppsSettings.tsx
@@ -1,6 +1,4 @@
-import { App } from '@/app/components/App';
 import { AppsHeader } from '@/settings/apps/components/AppsHeader';
-import React from 'react';
 
 export const AppsSettings = () => {
   return (
