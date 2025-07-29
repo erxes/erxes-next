4acff39d3fffb06ba917db37ffd0160d8f815cb9
bolortulga07 <bolortulga07@gmail.com>
2025-07-29 14:07:35 +0800

    periodLock model done

--- backend/plugins/finance_api/src/modules/saving/db/models/periodLockModel.ts
+++ backend/plugins/finance_api/src/modules/saving/db/models/periodLockModel.ts
@@ -1,11 +1,18 @@
 import { getFullDate } from 'erxes-api-shared/utils';
 import { FilterQuery, Model, models } from 'mongoose';
-import { IModels } from '~/connectionResolvers';
+import { IModels, loadClasses } from '~/connectionResolvers';
 import { CONTRACT_STATUS } from '~/modules/saving/@types/constants';
 import {
   IPeriodLock,
   IPeriodLockDocument,
 } from '~/modules/saving/@types/periodLockTypes';
+import {
+  IStoredInterest,
+  IStoredInterestDocument,
+} from '~/modules/saving/@types/storedInterest';
+import { periodLockSchema } from '~/modules/saving/db/definitions/periodLocks';
+import storedInterest from '~/modules/saving/db/utils/storedInterest';
+import { getDiffDay } from '~/modules/saving/db/utils/utils';
 
 export interface IPeriodLockModel extends Model<IPeriodLockDocument> {
   getPeriodLock(
@@ -23,9 +30,7 @@ export const loadPeriodLock = (models: IModels) => {
      * Get PeriodLock
      */
 
-    public static async getPeriodLock(
-      selector: FilterQuery<IPeriodLockDocument>,
-    ) {
+    public static async getPeriodLock(selector: FilterQuery<IPeriodLock>) {
       const periodLock = await models.PeriodLocks.findOne(selector);
 
       if (!periodLock) {
@@ -54,17 +59,54 @@ export const loadPeriodLock = (models: IModels) => {
 
       const nowDate = getFullDate(doc.date);
 
-      const contracts = await models.PeriodLocks.find({
+      const contracts = await models.Contracts.find({
         status: CONTRACT_STATUS.NORMAL,
         interestRate: { $gte: 0 },
       }).lean();
 
-      // for await (let contract of contracts) {
-      //   const lastStoredDate = getFullDate(contract.lastStoredDate);
-      //   const diffDay = getDiffDay(lastStoredDate, nowDate);
-      //   if (diffDay > 0) await storeInterest(contract, models, nowDate);
-      // }
-      return contracts;
+      for await (let contract of contracts) {
+        const lastStoredDate = getFullDate(contract.lastStoredDate);
+        const diffDay = getDiffDay(lastStoredDate, nowDate);
+        if (diffDay > 0) await storedInterest(contract, models, nowDate);
+      }
+      return periodLocks;
+    }
+
+    /**
+     * Update PeriodLock
+     */
+    public static async updatePeriodLock(_id: string, doc: IPeriodLock) {
+      await models.PeriodLocks.updateOne({ _id }, { $set: doc });
+
+      return models.PeriodLocks.findOne({ _id });
+    }
+
+    /**
+     * Remove PeriodLock
+     */
+    public static async removePeriodLock(_ids: string[]) {
+      const storedInterestList: IStoredInterestDocument[] =
+        await models.StoredInterest.find({ periodLock: { $in: _ids } }).lean();
+
+      for (const storedInterest of storedInterestList) {
+        await models.Contracts.updateOne(
+          { _id: storedInterest.contractId },
+          {
+            $set: {
+              storedInterest: { $inc: storedInterest.amount * -1 },
+              lastStoredDate: storedInterest.prevStoredDate,
+            },
+          },
+        );
+      }
+
+      await models.StoredInterest.deleteMany({
+        _id: storedInterestList.map((a) => a._id),
+      });
+
+      return models.PeriodLocks.deleteMany({ _id: { $in: _ids } });
     }
   }
+  periodLockSchema.loadClass(PeriodLock);
+  return periodLockSchema;
 };
--- backend/plugins/finance_api/src/modules/saving/db/models/storedInterestModel.ts
+++ backend/plugins/finance_api/src/modules/saving/db/models/storedInterestModel.ts
@@ -1,8 +1,13 @@
+import { getFullDate } from 'erxes-api-shared/utils';
 import { Model } from 'mongoose';
+import { IModels } from '~/connectionResolvers';
+import { CONTRACT_STATUS } from '~/modules/saving/@types/constants';
+import { IContractDocument } from '~/modules/saving/@types/contracts';
 import {
   IStoredInterest,
   IStoredInterestDocument,
 } from '~/modules/saving/@types/storedInterest';
+import { getDiffDay } from '~/modules/saving/db/utils/utils';
 
 export interface IStoredInterestModel extends Model<IStoredInterestDocument> {
   createStoredInterest(
@@ -11,7 +16,80 @@ export interface IStoredInterestModel extends Model<IStoredInterestDocument> {
   ): Promise<IStoredInterestDocument>;
   getStoredInterest(_id: string): Promise<IStoredInterestDocument>;
   updateStoredInterest(
-    _id,
+    _id: string,
     storedInterest: IStoredInterest,
   ): Promise<IStoredInterestDocument>;
 }
+
+export const loadSavingStoredInterestClass = (models: IModels) => {
+  class storedInterest {
+    public static async createStoredInterest(
+      payDate: Date,
+      periodLock: string,
+    ) {
+      const storeInterestDate = getFullDate(payDate);
+
+      const contracts: IContractDocument[] = await models.Contracts.find({
+        lastStoredDate: { $lt: storeInterestDate },
+        status: CONTRACT_STATUS.NORMAL,
+      }).lean();
+
+      if (contracts.length > 0) {
+        for (const contract of contracts) {
+          const prevSchedule: any = {};
+
+          let balanceAmount = 1;
+
+          if (prevSchedule) {
+            balanceAmount = prevSchedule.balance;
+          } else {
+            balanceAmount = contract.savingAmount;
+          }
+
+          let storedInterest = Number(
+            (
+              ((balanceAmount * contract.interestRate) / 100 / 365) *
+              getDiffDay(contract.lastStoredDate, storeInterestDate)
+            ).toFixed(0),
+          );
+
+          await models.StoredInterest.create({
+            amount: storedInterest,
+            contractId: contract._id,
+            invDate: storeInterestDate,
+            prevStoredDate: contract.lastStoredDate,
+            periodLockId: periodLock,
+            number: contract.number,
+          });
+
+          await models.Contracts.updateOne(
+            { _id: contract._id },
+            {
+              $inc: { storedInterest: storedInterest },
+              $set: { lastStoredDate: storeInterestDate },
+            },
+          );
+        }
+      }
+
+      return {};
+    }
+
+    public static async getStoredInterest(
+      _id: string,
+      storedInterest: IStoredInterest,
+    ) {
+      return await models.StoredInterest.findOne({ _id }).lean();
+    }
+
+    public static async updateStoredInterest(
+      _id: string,
+      storedInterest: IStoredInterest,
+    ) {
+      return await models.StoredInterest.updateOne(
+        { _id },
+        { $set: storedInterest },
+      );
+    }
+  }
+};
