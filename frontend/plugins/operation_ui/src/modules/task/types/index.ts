7ca20fd057a230843b65c284c3398d131538ee63
Kato-101 <gundsambuuganzil111@gmail.com>
2025-08-11 15:51:15 +0800

    Add GraphQL query and hook for fetching status choices by team

---
+++ frontend/plugins/operation_ui/src/modules/task/types/index.ts
@@ -0,0 +1,34 @@
+import { addTaskSchema } from '@/task/types/validations';
+import { z } from 'zod';
+import { TeamStatusTypes } from '@/team/types';
+
+export interface IEstimateChoice {
+  label: string;
+  value: number;
+}
+
+export interface ITask {
+  _id: string;
+  name: string;
+  tagIds: string[];
+  createdAt: string;
+  priority: number;
+  status: number;
+  targetDate: string;
+  assigneeId: string;
+  teamId: string;
+  projectId: string;
+  estimatedPoint: number;
+  estimateChoices: IEstimateChoice[];
+}
+
+export interface IStatus {
+  value: number;
+  label: string;
+  color: string;
+  type: TeamStatusTypes;
+}
+
+
+export type TAddTask = z.infer<typeof addTaskSchema>;
+export * from '@/task/types/validations';
