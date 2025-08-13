aa884945039dee9e097b0846f718d721470d9267
uuganaa1007 <suuganbayr948@gmail.com>
2025-08-13 17:02:42 +0800

    chore change

--- backend/plugins/frontline_api/src/modules/integrations/facebook/middlewares/loginMiddleware.ts
+++ backend/plugins/frontline_api/src/modules/integrations/facebook/middlewares/loginMiddleware.ts
@@ -111,7 +111,7 @@ export const loginMiddleware = async (req, res) => {
     const reactAppUrl = !DOMAIN.includes('zrok')
       ? DOMAIN
       : 'http://localhost:3001';
-    const url = `${reactAppUrl}/settings/fb-authorization?fbAuthorized=true`;
+    const url = `${reactAppUrl}/settings/inbox/integrations/facebook-messenger?fbAuthorized=true`;
 
     debugResponse(debugFacebook, req, url);
 
