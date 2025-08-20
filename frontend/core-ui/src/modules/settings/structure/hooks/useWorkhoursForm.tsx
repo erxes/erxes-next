7192f734394a02420b1451b282a46314444ac713
Bilguun Munkhzul <kazue@Bilguuns-MacBook-Pro.local>
2025-08-20 13:38:49 +0800

    chore: fix

--- frontend/core-ui/src/modules/settings/structure/hooks/useWorkhoursForm.tsx
+++ frontend/core-ui/src/modules/settings/structure/hooks/useWorkhoursForm.tsx
@@ -1,5 +1,5 @@
 import { workingHours } from '@/settings/structure/constants/work-days';
-import { workingHoursSchema } from '@/settings/structure/schemas/workHoursSchema';
+import { WORKING_HOURS_SCHEMA } from '@/settings/structure/schemas/workHoursSchema';
 import { IWorkhoursForm } from '@/settings/structure/types/workhours';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { useForm } from 'react-hook-form';
@@ -8,7 +8,7 @@ export const useWorkhoursForm = () => {
   const form = useForm<IWorkhoursForm>({
     mode: 'onBlur',
     defaultValues: workingHours,
-    resolver: zodResolver(workingHoursSchema),
+    resolver: zodResolver(WORKING_HOURS_SCHEMA),
   });
   return { form };
 };
