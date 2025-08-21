d491df79f17bbf42dbfa1b24dc2115091f1bc93b
munkhsaikhan <munkhsaikhan.g@nmma.co>
2025-08-21 17:19:27 +0800

    fix: navigation

--- frontend/plugins/accounting_ui/src/modules/AccountingNavigation.tsx
+++ frontend/plugins/accounting_ui/src/modules/AccountingNavigation.tsx
@@ -4,18 +4,17 @@ import { NavigationMenuGroup, NavigationMenuLinkItem } from 'erxes-ui';
 export const AccountingNavigation = () => {
   return (
     <>
-      {/* <NavigationMenuLinkItem name="" icon={IconCashBanknote} path="accounting" /> */}
       <NavigationMenuLinkItem name='Accounting' icon={IconCashBanknote} path="accounting/main" />
       <NavigationMenuLinkItem name='records' icon={IconCashBanknote} path="accounting/records" />
-      <NavigationMenuLinkItem name='odds' icon={IconCashBanknote} path="accounting/odd-transactions" />
       <div className='-mt-3'>
-        <NavigationMenuGroup name='Adjustment' separate={false}>
+        <NavigationMenuGroup name='Adjustment' separate={false} defaultOpen={false}>
           <NavigationMenuLinkItem name='Fund Rate' icon={IconCashBanknote} path="accounting/adjustment/fundRate"></NavigationMenuLinkItem>
           <NavigationMenuLinkItem name='Debt Rate' icon={IconCashBanknote} path="accounting/adjustment/debRate"></NavigationMenuLinkItem>
           <NavigationMenuLinkItem name='Inventory Cost' icon={IconCashBanknote} path="accounting/adjustment/inventory"></NavigationMenuLinkItem>
           <NavigationMenuLinkItem name='FixedAsset' icon={IconCashBanknote} path="accounting/adjustment/fxa"></NavigationMenuLinkItem>
         </NavigationMenuGroup>
       </div>
+      <NavigationMenuLinkItem name='odds' icon={IconCashBanknote} path="accounting/odd-transactions" />
     </>
   );
 };
