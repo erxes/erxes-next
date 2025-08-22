cc2632287d56668d35d0c74069c97331bc2b912d
Kato-101 <gundsambuuganzil111@gmail.com>
2025-08-22 12:40:46 +0800

    feat: replace CommentField with NotesField and add INote interface

--- frontend/plugins/operation_ui/src/modules/task/components/detail/TaskFields.tsx
+++ frontend/plugins/operation_ui/src/modules/task/components/detail/TaskFields.tsx
@@ -16,7 +16,7 @@ import { useGetCurrentUsersTeams } from '@/team/hooks/useGetCurrentUsersTeams';
 import { ITask } from '@/task/types';
 import { ActivityList } from '@/activity/components/ActivityList';
 import { SelectTaskPriority } from '@/task/components/select/SelectTaskPriority';
-import { CommentField } from '@/task/components/CommentField';
+import { NotesField } from '@/task/components/NotesField';
 
 export const TaskFields = ({ task }: { task: ITask }) => {
   const {
@@ -129,7 +129,7 @@ export const TaskFields = ({ task }: { task: ITask }) => {
         />
       </div>
       <ActivityList contentId={taskId} contentDetail={task} />
-      <CommentField />
+      <NotesField taskId={taskId} />
     </div>
   );
 };
