c88ecb9b788c7600257ccb4cc1407e1d89934f55
soyombo <e11iot.soko@gmail.com>
2025-08-28 22:40:18 +0800

    Update vite.config.ts

--- backend/plugins/payment_api/src/widget/vite.config.ts
+++ backend/plugins/payment_api/src/widget/vite.config.ts
@@ -1,7 +1,7 @@
 import react from '@vitejs/plugin-react-swc';
 import { defineConfig } from 'vite';
 
-import path from 'path';
+import * as path from 'path';
 
 export default defineConfig({
   plugins: [react()],
