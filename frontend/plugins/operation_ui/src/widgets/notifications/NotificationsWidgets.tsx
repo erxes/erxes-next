990ff756dd917c0cda357ffd5c7b9a18cebd9ede
batmnkh2344 <batmnkh2344@gmail.com>
2025-09-24 16:13:46 +0800

    chore: system content for plugin

--- frontend/plugins/operation_ui/src/widgets/notifications/NotificationsWidgets.tsx
+++ frontend/plugins/operation_ui/src/widgets/notifications/NotificationsWidgets.tsx
@@ -1,7 +1,24 @@
 import { TaskDetails } from '@/task/components/detail/TaskDetails';
+import { NotificationContent } from './contents/NotificationContent';
 
 const NotificationsWidgets = (props: any) => {
-  const { contentTypeId } = props;
+  const { contentTypeId, contentType } = props;
+
+  const [_, moduleName, collectionType] = (contentType || '')
+    .replace(':', '.')
+    .split('.');
+
+  if (moduleName === 'system') {
+    const NotificationComponent =
+      NotificationContent[collectionType as keyof typeof NotificationContent];
+
+    if (!NotificationComponent) {
+      return <></>;
+    }
+
+    return <NotificationComponent {...props} />;
+  }
+
   return <TaskDetails taskId={contentTypeId} />;
 };
 
