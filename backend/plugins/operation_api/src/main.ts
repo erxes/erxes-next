8ea8b05e396ba6c70a8b88bd7238063c63939740
Enkhtuvshin Narmandakh <37796969+Enkhtuvshin0513@users.noreply.github.com>
2025-10-01 14:59:25 +0800

    upd

--- backend/plugins/operation_api/src/main.ts
+++ backend/plugins/operation_api/src/main.ts
@@ -1,66 +1,11 @@
-import { redis, startPlugin } from 'erxes-api-shared/utils';
-import { typeDefs } from '~/apollo/typeDefs';
-import resolvers from './apollo/resolvers';
-import { generateModels } from './connectionResolvers';
-import { Router } from 'express';
-import { initMQWorkers } from '~/worker';
+const express = require('express');
+const app = express();
+const port = 3000;
 
-export const router: Router = Router();
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
 
-startPlugin({
-  name: 'operation',
-  port: 3306,
-  graphql: async () => ({
-    typeDefs: await typeDefs(),
-    resolvers,
-  }),
-  hasSubscriptions: true,
-  subscriptionPluginPath: require('path').resolve(
-    __dirname,
-    'apollo',
-    process.env.NODE_ENV === 'production'
-      ? 'subscription.js'
-      : 'subscription.ts',
-  ),
-  apolloServerContext: async (subdomain, context) => {
-    const models = await generateModels(subdomain);
-
-    context.models = models;
-
-    return context;
-  },
-  onServerInit: async () => {
-    await initMQWorkers(redis);
-  },
-
-  expressRouter: router,
-
-  meta: {
-    notificationModules: [
-      {
-        name: 'tasks',
-        description: 'Tasks',
-        icon: 'IconChecklist',
-        types: [
-          { name: 'taskAssignee', text: 'Task assignee' },
-          { name: 'taskStatus', text: 'Task status changed' },
-        ],
-      },
-      {
-        name: 'projects',
-        description: 'Projects',
-        icon: 'IconClipboard',
-        types: [
-          { name: 'projectAssignee', text: 'Project assignee' },
-          { name: 'projectStatus', text: 'Project status changed' },
-        ],
-      },
-      {
-        name: 'note',
-        description: 'Note',
-        icon: 'IconNote',
-        types: [{ name: 'note', text: 'Mentioned in note' }],
-      },
-    ],
-  },
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
 });
